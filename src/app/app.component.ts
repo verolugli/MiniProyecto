73b67d96b4101e44287a5052fa48b37e25279c6e
Veronica <veronicalugli@correo.unimet.edu.ve>
2018-10-15 22:49:36 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {  PokedexService } from './pokedex.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pokemon-app';
+
+   pokemon;
+
+  constructor(service: PokedexService) {
+     this.pokemon = service.getPokemon();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { PokedexService } from './pokedex.service';
+import { DescripcionComponent } from './descripcion/descripcion.component';
+import { InformacionComponent } from './informacion/informacion.component';
+import { HomeComponent } from './home/home.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { RouterModule, Routes } from '@angular/router';
 
-@NgModule({
+const appRoutes: Routes = [
+  { path: 'informacion', component: InformacionComponent },
+
+
+];
+
+@NgModule(  {
   declarations: [
-    AppComponent
+    AppComponent,
+    DescripcionComponent,
+    InformacionComponent,
+    EncabezadoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
-  providers: [],
+  providers: [
+    PokedexService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
